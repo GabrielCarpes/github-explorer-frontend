21315e0b98ec29ff0434d23c2e9d84ee7467c575
Gabriel Carpes <gabrcarpes@gmail.com>
2021-03-03 19:54:28 -0300

    create branch development

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function App() {
-  return <h1>GEGedaS</h1>;
-}
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Development</h1>;
+    </>
+  );
+};
 
 export default App;
